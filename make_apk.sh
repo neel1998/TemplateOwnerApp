2847066f0259fbee493b1c113c1c09288dcf96ee
neel1998 <neellovepainting@gmail.com>
2020-07-12 14:12:30 +0530

    make apk script updated

--- make_apk.sh
+++ make_apk.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+mainPath=/home/neel/AndroidStudioProjects/TemplateOwnerApp
 
-p=/home/neel/AndroidStudioProjects/TemplateOwnerApp/app/src/main/assets/meta_data.txt
+p=$mainPath/app/src/main/assets/meta_data.txt
 #echo $p
 echo $1 > $p
 
 appName='2s|.*|<string name="app_name">'$2'</string>|'
 
-sed -i "$appName" /home/neel/AndroidStudioProjects/TemplateOwnerApp/app/src/main/res/values/strings.xml
+sed -i "$appName" $mainPath/app/src/main/res/values/strings.xml
 
 id='7s/.*/        applicationId "com.abc.'$2'"/'
-sed -i "$id" app/build.gradle
+sed -i "$id" $mainPath/app/build.gradle
 
-./gradlew assembleDebug
+bash $mainPath/gradlew assembleDebug
 
-mv /home/neel/AndroidStudioProjects/TemplateOwnerApp/app/build/outputs/apk/debug/app-debug.apk /home/neel/AndroidStudioProjects/TemplateOwnerApp/app/build/outputs/apk/debug/$2.apk
+mv $mainPath/app/build/outputs/apk/debug/app-debug.apk $mainPath/app/build/outputs/apk/debug/$2.apk
